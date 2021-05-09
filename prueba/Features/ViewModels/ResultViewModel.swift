b70a588d30dba87795f1d0a3bd42c4979a27582b
XaviRoigAznar97 <xavi.roig97@gmail.com>
2021-05-09 19:03:45 +0200

    valid chars + tableview to horizontal margins

--- prueba/Features/ViewModels/ResultViewModel.swift
+++ prueba/Features/ViewModels/ResultViewModel.swift
@@ -11,7 +11,7 @@ class ResultViewModel {
 
     var manager = WordSearcherManager.sharedInstance
     
-    static var validSingleChars = ["A","E","I","O","U","0","1","2","3","4","5","6","7","8","9"]
+    static let validSingleChars = ["A","E","I","O","U","0","1","2","3","4","5","6","7","8","9"]
     
     fileprivate (set) var selectedBtnType: ResultPageFilterButtonType?
     
